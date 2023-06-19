84bd935ed7ed0591ac9d633d3b1ce184882a281a
shreeja <shreejayashri@gmail.com>
2023-06-20 01:06:46 +0530

    first commit

---
+++ src/components/Skills.js
@@ -0,0 +1,66 @@
+import Carousel from "react-multi-carousel";
+import "react-multi-carousel/lib/styles.css";
+import meter1 from "../assets/img/meter1.png";
+import meter2 from "../assets/img/meter2.png";
+import meter3 from "../assets/img/meter3.png";
+
+export const Skills =() =>{
+    const responsive = {
+        superLargeDesktop: {
+          // the naming can be any, depends on you.
+          breakpoint: { max: 4000, min: 3000 },
+          items: 5
+        },
+        desktop: {
+          breakpoint: { max: 3000, min: 1024 },
+          items: 3
+        },
+        tablet: {
+          breakpoint: { max: 1024, min: 464 },
+          items: 2
+        },
+        mobile: {
+          breakpoint: { max: 464, min: 0 },
+          items: 1
+        }
+      };
+      
+      return(
+        <section className="skill" id="skills">
+        <div className="container">
+            <div className="row">
+                <div className="col-12">
+                <div className="skill-bx wow zoomIn">
+                    <h2>
+                        Skills
+                    </h2>
+                    <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer facilisis faucibus nulla, in elementum nulla maximus ut. Morbi elementum placerat vulputate.
+                    </p>
+                    <Carousel responsive={responsive} infinite={true} className="owl-carousel owl-theme skill-slider">
+                        <div className="item">
+                            <img src={meter1} alt="Image"/>
+                            <h5> Web Development</h5>
+                        </div>
+                        <div className="item">
+                            <img src={meter2} alt="Image"/>
+                            <h5> Brand Identify</h5>
+                        </div>
+                        <div className="item">
+                            <img src={meter3} alt="Image"/>
+                            <h5>Logo Designing</h5>
+                        </div>
+                        <div className="item">
+                            <img src={meter1} alt="Image"/>
+                            <h5> Web Development</h5>
+                        </div>
+                    </Carousel>
+                </div>
+                </div>
+            </div>
+        </div>
+      
+        
+        </section>
+      )
+}
